fc330bbe23be73295667dd1ce65928052993b336
Scott Schmidt <sjschmidt44@gmail.com>
2015-06-23 13:33:19 -0700

    Create Entry class for post framework.

---
+++ journal.py
@@ -0,0 +1,53 @@
+# _*_ coding: utf-8 _*_
+from __future__ import unicode_literals
+import os
+from pyramid.config import Configurator
+from pyramid.view import view_config
+from waitress import serve
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+
+Base = declarative_base()
+
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    title = sa.Column(sa.Unicode(128), nullable=False)
+    text = sa.Column(sa.Unicode)
+    timestamp = sa.Column(
+        sa.DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return "<Entry(title='%s', timestamp='%s')>" % (
+            self.title, self.timestamp
+        )
+
+
+@view_config(route_name='home', renderer='string')
+def home(request):
+    return 'Hello World'
+
+
+def main():
+    """Create a configured wsgi app"""
+    settings = {}
+    debug = os.environ.get('DEBUG', True)
+    settings['reload_all'] = debug
+    settings['debug_all'] = debug
+    # config setup
+    config = Configurator(
+        settings=settings
+    )
+    config.add_route('home', '/')
+    config.scan()
+    app = config.make_wsgi_app()
+    return app
+
+
+if __name__ == '__main__':
+    app = main()
+    port = os.environ.get('PORT', 5000)
+    serve(app, host='0.0.0.0', port=port)
